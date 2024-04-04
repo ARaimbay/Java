bf91fb47aadc8d302568d815bd6ec1f6412a469b
Aigerim Raimbay <raimbay89@gmail.com>
2024-04-04 15:38:06 -0500

    added psvm for cat to call toString method

--- src/model/Cat.java
+++ src/model/Cat.java
@@ -4,4 +4,9 @@ public class Cat extends Pet{
     public void meow() {
         System.out.println("Meow meow");
     }
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        System.out.println(cat1);
+    }
 }

fd6c3604eaf337258a87986d4758a461764c7d5a
Aigerim Raimbay <raimbay89@gmail.com>
2024-04-05 16:49:48 -0500

    private vs public methods

--- src/package2/ClassB.java
+++ src/package2/ClassB.java
@@ -3,5 +3,10 @@ package package2;
 public class ClassB {
     public void publicMethod() {
         System.out.println("This is a public method of ClassB.");
+        privateMethod();
+    }
+
+    private void privateMethod() {
+        System.out.println("This is the private method of ClassB");
     }
 }
